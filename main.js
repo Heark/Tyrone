e0b708da6768935590501b9ad941193b0bb8682c
Heark <adtune6509@student.waynecc.edu>
2017-04-10 06:12:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var Discordie = require('discordie');
 // The auto response AI for Tyrone
 var RiveScript = require("rivescript");
 var TyroneAI = new RiveScript();
+var getJSON = require('get-json')
 
 const Event = Discordie.Events;
 const Tyrone = new Discordie();
@@ -11,6 +12,9 @@ const Tyrone = new Discordie();
 // See how fast the boat is loaded.
 var loading = new Date().getTime();
 var load_time;
+
+// Command symbol used to detect when a command is being used.
+var Command_Symbol = "!"
 
 // Load rivescript replies.
 TyroneAI.loadDirectory("AI", loading_complete, load_error);
@@ -38,7 +42,7 @@ function load_error(error) {
 Tyrone.connect({
     token: 'MzAwODczNTk0OTYyMDUxMDcz.C8yyOA.uzKHOizKOZxWjeARU0As5r-jldc'
 })
-
+var commands = ["define"]
 
 Tyrone.Dispatcher.on(Event.GATEWAY_READY, e => {
     // Get how many milliseconds have passed since the loading variable was loaded.
@@ -52,5 +56,35 @@ Tyrone.Dispatcher.on(Event.GATEWAY_READY, e => {
 
 // Event listener for when a new message is created.
 Tyrone.Dispatcher.on(Event.MESSAGE_CREATE, e => {
+    // Detects if the first character of the message is the command symbol
+    if (e.message.content.toLowerCase().charAt(0) == Command_Symbol) {
+        // gets the user input after the command
+        var userdata = e.message.content.substring(e.message.content.indexOf(" ") + 1);
+        // checks if the message is an actual command probably a really slow method but hey
+        for (var i = 0; i < commands.length; i++) {
+            if (e.message.content.substring(1, commands[i].length + 1) == commands[i]) {
+                var command = e.message.content.substring(1, commands[i].length + 1);
+
+                // Commands start here, must be added to the commands array as well.
+
+                if (command == "define") {
+                    var word = userdata;
+                    getJSON("http://api.urbandictionary.com/v0/define?term=" + word, function(error, c) {
+                        var getword = c.list[0].word,
+                            word = getword.charAt(0).toUpperCase() + getword.substring(1),
+                            definition = c.list[0].definition,
+                            out = "```" + word + ":``` " + definition;
+                        e.message.channel.sendMessage(out);
+                    });
+                }
+
+
+
+
+            } else {
+                e.message.channel.sendMessage("LMAO that command doesn't exist. Homie.")
+            };
+        }
+    }
 
 })
