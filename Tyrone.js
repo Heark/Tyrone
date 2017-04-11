2fd094639624c80cc94cd853bc8620cfc1176642
Heark <adtune6509@student.waynecc.edu>
2017-04-11 18:24:44 -0400

    added two new spotify commands

--- Tyrone.js
+++ Tyrone.js
@@ -20,11 +20,13 @@ var fs = require('fs'),
     request = require('request');
 
 // Best Pokemon API for the !pokemon command.
-var Pokedex = require("pokedex-promise-v2")
+var Pokedex = require("pokedex-promise-v2");
 
 
 // IMDB api for the !imdb command
 const imdb = require('imdb-api');
+
+
 
 // Constants for the actual bot.
 const Event = Discordie.Events;
@@ -37,7 +39,7 @@ var SpotifyWebApi = require('spotify-web-api-node');
 
 
 // Command symbol used to detect when a command is being used.
-var Command_Symbol = "!"
+var Command_Symbol = "!";
 
 // Useful functions to be used.
 // capitalizes first letter in a string.
@@ -65,7 +67,11 @@ var download = function(uri, filename, callback) {
 
 };
 
+// Formats a number to a string using regex and adds commas marking the thousandths place and such
+function format(num) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
+}
 
 // Load rivescript replies.
 TyroneAI.loadDirectory("AI", loading_complete, load_error);
@@ -89,7 +95,7 @@ function loading_complete(batch_num) {
                 e.message.channel.sendMessage(reply);
             }
         }
-    })
+    });
 
 }
 
@@ -99,8 +105,8 @@ function load_error(error) {
 
 Tyrone.connect({
     token: 'INSERT YOUR TOKEN HERE'
-})
-var commands = ["define", "character", "imdb", "pokemon", "spotify"];
+});
+var commands = ["define", "character", "imdb", "pokemon", "spotify", "nba"];
 
 Tyrone.Dispatcher.on(Event.GATEWAY_READY, e => {
     // Get how many milliseconds have passed since the loading variable was loaded.
@@ -110,7 +116,7 @@ Tyrone.Dispatcher.on(Event.GATEWAY_READY, e => {
     // Print to the console when the bot has been loaded.
     console.log('Bot Online! Connected in ' + load_time + ' seconds.');
 
-})
+});
 
 // Event listener for when a new message is created.
 Tyrone.Dispatcher.on(Event.MESSAGE_CREATE, e => {
@@ -129,113 +135,143 @@ Tyrone.Dispatcher.on(Event.MESSAGE_CREATE, e => {
 
         // gets the substring in between characters in this case the text between the command symbol and the first space
         var command = e.message.content.split(Command_Symbol).pop().split(' ').shift();
+        // boolean for commands with cooldowns
+        var cooldown = false;
         // checks if the message is an actual command by checking if the command string is in the commands array.
         if (inArray(commands, command) == true) {
             // Commands start here, must be added to the commands array as well.
 
             if (command == "define") {
-                    var word = userdata;
-                    getJSON("http://api.urbandictionary.com/v0/define?term=" + word, function(error, c) {
-                        var getword = c.list[0].word,
-                            word = getword.charAt(0).toUpperCase() + getword.substring(1),
-                            definition = c.list[0].definition,
-                            out = "```" + word + ":``` " + definition;
-                        e.message.channel.sendMessage(out);
-                    });
-                
+                var word = userdata;
+                getJSON("http://api.urbandictionary.com/v0/define?term=" + word, function(error, c) {
+                    var getword = c.list[0].word,
+                        word = getword.charAt(0).toUpperCase() + getword.substring(1),
+                        definition = c.list[0].definition,
+                        out = "```" + word + ":``` " + definition;
+                    e.message.channel.sendMessage(out);
+                });
+
             } else if (command == "character") {
-                    var name = userdata.toLowerCase();
-                    Character.quickSearch(name).then(function(results) {
-                        // access and fetch the first character
-                        results.character[0].fetch().then(function(r) {
-                            // access and fetch the first anime
-                            r.animeography[0].fetch().then(function(r) {
-                                console.log(r.pictures[0]);
-                                // download the first picture
-                                download(r.pictures[0], '' + name + '.jpg', function() {
-                                    console.log('Downloaded image of ' + name);
-                                });
-                                // Tyrone will try to upload the image before it is downloaded so we set it on a 5 second time out.
-                                setTimeout(function() {
-                                    e.message.channel.uploadFile('' + name + '.jpg', null, " " + upcase(name) + " is a character from the anime: " + r.title, true);
-                                    e.message.channel.sendMessage(r.description);
-                                }, 5000);
-
-
-                            })
-                        });
-                    });
-                
-            } else if (command == "imdb") {
-                    var moviename = userdata.toLowerCase();
-                    imdb.getReq({
-                        name: moviename
-                    }, (err, things) => {
-                        var movie = things;
-                        download(things.poster, '' + moviename + '.jpg', function() {
-                            console.log('Downloaded image of ' + moviename);
-                        });
-                        // Again preventing upload before the image is downloaded.
-                        setTimeout(function() {
-                            e.message.channel.uploadFile('' + moviename + '.jpg');
-                            e.message.channel.sendMessage(things.plot);
-                            e.message.channel.sendMessage('Rating: ' + things.rating);
-                        }, 5000);
-                    });
-                
-
-            } else if (command == "pokemon") {
-                    var Pokemon = new Pokedex();
-                    var pokemon_name = userdata.toLowerCase();
-                    Pokemon.getPokemonByName(pokemon_name)
-                        .then(function(response) {
-                            // once Again preventing upload before the image is downloaded.
-                            download(response.sprites.front_default, '' + pokemon_name + '.png', function() {
-                                console.log('Downloaded image of ' + pokemon_name);
+                var name = userdata.toLowerCase();
+                Character.quickSearch(name).then(function(results) {
+                    // access and fetch the first character
+                    results.character[0].fetch().then(function(r) {
+                        // access and fetch the first anime
+                        r.animeography[0].fetch().then(function(r) {
+                            console.log(r.pictures[0]);
+                            // download the first picture
+                            download(r.pictures[0], '' + name + '.jpg', function() {
+                                console.log('Downloaded image of ' + name);
                             });
-                            var POKEMONDATA = response
+                            // Tyrone will try to upload the image before it is downloaded so we set it on a 5 second time out.
                             setTimeout(function() {
-                                e.message.channel.uploadFile('' + pokemon_name + '.png');
-                                e.message.channel.sendMessage(upcase(pokemon_name));
-                                console.log("Getting information on " + pokemon_name);
-                                e.message.channel.sendMessage('Base Stats: ');
-                                e.message.channel.sendMessage(upcase(POKEMONDATA.stats[0].stat.name) + ': ' + POKEMONDATA.stats[0].base_stat);
-                                e.message.channel.sendMessage(upcase(POKEMONDATA.stats[1].stat.name) + ': ' + POKEMONDATA.stats[1].base_stat);
-                                e.message.channel.sendMessage(upcase(POKEMONDATA.stats[2].stat.name) + ': ' + POKEMONDATA.stats[2].base_stat);
-                                e.message.channel.sendMessage(upcase(POKEMONDATA.stats[3].stat.name) + ': ' + POKEMONDATA.stats[3].base_stat);
-                                e.message.channel.sendMessage(upcase(POKEMONDATA.stats[4].stat.name) + ': ' + POKEMONDATA.stats[4].base_stat);
-                                e.message.channel.sendMessage(upcase(POKEMONDATA.stats[5].stat.name) + ': ' + POKEMONDATA.stats[5].base_stat);
+                                e.message.channel.uploadFile('' + name + '.jpg', null, " " + upcase(name) + " is a character from the anime: " + r.title, true);
+                                e.message.channel.sendMessage(r.description);
                             }, 5000);
 
-                        })
-                        .catch(function(error) {
-                            e.message.channel.sendMessage('LMFAOOOOOOOOOO There ain\'t no pokemon called ' + pokemon_name + '.');
+
                         });
+                    });
+                });
+
+            } else if (command == "imdb") {
+                var moviename = userdata.toLowerCase();
+                imdb.getReq({
+                    name: moviename
+                }, (err, things) => {
+                    download(things.poster, '' + moviename + '.jpg', function() {
+                        console.log('Downloaded image of ' + moviename);
+                    });
+                    // Again preventing upload before the image is downloaded.
+                    setTimeout(function() {
+                        e.message.channel.uploadFile('' + moviename + '.jpg');
+                        e.message.channel.sendMessage(things.plot);
+                        e.message.channel.sendMessage('Rating: ' + things.rating);
+                    }, 5000);
+                });
+
+
+            } else if (command == "pokemon") {
+                var Pokemon = new Pokedex();
+                var pokemon_name = userdata.toLowerCase();
+                Pokemon.getPokemonByName(pokemon_name)
+                    .then(function(response) {
+                        // once Again preventing upload before the image is downloaded.
+                        download(response.sprites.front_default, '' + pokemon_name + '.png', function() {
+                            console.log('Downloaded image of ' + pokemon_name);
+                        });
+                        var POKEMONDATA = response;
+                        setTimeout(function() {
+                            e.message.channel.uploadFile('' + pokemon_name + '.png');
+                            e.message.channel.sendMessage(upcase(pokemon_name));
+                            console.log("Getting information on " + pokemon_name);
+                            e.message.channel.sendMessage('Base Stats: ');
+                            e.message.channel.sendMessage(upcase(POKEMONDATA.stats[0].stat.name) + ': ' + POKEMONDATA.stats[0].base_stat);
+                            e.message.channel.sendMessage(upcase(POKEMONDATA.stats[1].stat.name) + ': ' + POKEMONDATA.stats[1].base_stat);
+                            e.message.channel.sendMessage(upcase(POKEMONDATA.stats[2].stat.name) + ': ' + POKEMONDATA.stats[2].base_stat);
+                            e.message.channel.sendMessage(upcase(POKEMONDATA.stats[3].stat.name) + ': ' + POKEMONDATA.stats[3].base_stat);
+                            e.message.channel.sendMessage(upcase(POKEMONDATA.stats[4].stat.name) + ': ' + POKEMONDATA.stats[4].base_stat);
+                            e.message.channel.sendMessage(upcase(POKEMONDATA.stats[5].stat.name) + ': ' + POKEMONDATA.stats[5].base_stat);
+                        }, 5000);
+
+                    })
+                    .catch(function(error) {
+                        e.message.channel.sendMessage('LMFAOOOOOOOOOO There ain\'t no pokemon called ' + pokemon_name + '.');
+                    });
             } else if (command == "spotify") {
 
-            if(userdata == "!spotify"){
-                e.message.channel.sendMessage("Ok... so what you want me to do.");
-            } else {
-                var Spotify = new SpotifyWebApi();
+                if (userdata == "!spotify") {
+                    e.message.channel.sendMessage("Ok... so what you want me to do.");
+                } else {
+                    var Spotify = new SpotifyWebApi();
+                    var sub_command = userdata.toLowerCase().replace(userdata_2.toLowerCase(), "").trimRight();
+                    console.log(sub_command);
+                    if (sub_command == "playlistsearch") {
+                        var playlist = userdata_2.toLowerCase();
+                        Spotify.searchPlaylists(playlist, {
+                                limit: 50
+                            })
+                            .then(function(data) {
+                                e.message.channel.sendMessage('Yo I Found this playlist:');
+                                e.message.channel.sendMessage(data.body.playlists.items[Math.floor(Math.random() * 50) + 0].external_urls.spotify);
+                            }, function(err) {
 
-                var sub_command = userdata.toLowerCase().replace(userdata_2, "").replace(" ", "");
-                if (sub_command == "playlistsearch") {
-                    var playlist = userdata_2.toLowerCase();
-                    Spotify.searchPlaylists(playlist)
-                        .then(function(data) {
-                            e.message.channel.sendMessage('Yo I Found this playlists:');
-                            e.message.channel.sendMessage(data.body.playlists.items[Math.floor(Math.random() * 20) + 0  
-].external_urls.spotify);
-                        }, function(err) {
-                            e.message.channel.sendMessage('Damn, I got nothing');
-                        });
+                                e.message.channel.sendMessage('Damn, I got nothing');
+                            });
+
+                    } else if (sub_command == "song") {
+                        var song = userdata_2.toLowerCase();
+                        Spotify.searchTracks(song)
+                            .then(function(data) {
+                                e.message.channel.sendMessage('YO is that this song');
+                                e.message.channel.sendMessage(data.body.tracks.items[0].external_urls.spotify);
+                            }, function(err) {
+                                console.error(err);
+                            });
+
+                    } else if (sub_command == "user") {
+                        var user = userdata_2.toLowerCase();
+                        Spotify.getUser(user)
+                            .then(function(data) {
+                                // if there is no display name do nothing.
+                                if (data.body.display_name == null || undefined) {
+
+                                } else {
+                                    e.message.channel.sendMessage(data.body.display_name);
+                                }
+                                e.message.channel.sendMessage('Followers: ' + format(data.body.followers.total));
+                                e.message.channel.sendMessage(data.body.external_urls.spotify);
+                            }, function(err) {
+                                e.message.channel.sendMessage('lol  who?');
+                            });
+
+                    }
                 }
             }
-        }
 
         } else {
-            e.message.channel.sendMessage("LMAO the command " + command + " doesn't exist homie.")
-        };
+            e.message.channel.sendMessage("LMAO the command " + command + " doesn't exist homie.");
+        }
 
     }
 
