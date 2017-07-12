4f3afa665a4d6fd85d797c3a742f52e594bf7ce0
Heark <adtune6509@student.waynecc.edu>
2017-07-12 07:45:53 -0400

    Update Tyrone.js

--- Tyrone.js
+++ Tyrone.js
@@ -87,12 +87,6 @@ function FileExists(FileName) {
         }
     });
 }
-gulp.task('start', function () {
-  nodemon({
-    script: 'Tyrone.js'
-  , env: { 'NODE_ENV': 'development' }
-  })
-})
 
 // Load rivescript replies.
 TyroneAI.loadDirectory("AI", loading_complete, load_error);
